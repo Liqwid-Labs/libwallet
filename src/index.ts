9e3f1c3940dc997cb9faaa4f8d26978a56f5f154
Banou <me@banou.dev>
2022-10-07 02:35:58 +0200

    chore: 🔥remove wrapOffchain

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import type { BaseAddress } from '@emurgo/cardano-serialization-lib-browser'
 
-import PQueue from 'p-queue'
 import { makeEventEmitter } from './utils/events'
 import { getWalletImpl, SupportedWalletId } from './wallets'
 
@@ -31,61 +30,4 @@ export const makeWallet = async <T extends SupportedWalletId>({ id }: { id: T })
 
   walletImpl.wallet.init({ wallet, api })
   return wallet
-}
-
-export interface WrapOffchainContextOptions<T> {
-  init: ({ wallet }: { wallet?: Wallet }) => Promise<T>
-  terminate: (context: Awaited<ReturnType<WrapOffchainContextOptions<T>['init']>>) => Promise<void>
-}
-
-type WrapOffchainOptions<T> =
-  WrapOffchainContextOptions<T>
-  & {
-    queue?: boolean
-    wallet?: Wallet
-  }
-
-type WrapFunctionOptions = {
-  queue?: boolean
-}
-
-
-export type OmitFirstArg<F> = F extends (x: any, ...args: infer P) => infer R ? (...args: P) => R : never;
-
-export const wrapOffchain = <T>(options: WrapOffchainOptions<T>) => {
-  const queue = new PQueue({ concurrency: options.queue ? 1 : Infinity })
-  let wallet: Wallet | undefined = options.wallet
-  let context: T | undefined
-
-  const getContext = async (): Promise<T> => {
-    if (!context) context = await (options as WrapOffchainContextOptions<T>).init({ wallet })
-    return context
-  }
-
-  const wrapFunction =
-    <T2 extends (context: T, ...args: any[]) => any>(func: T2, options?: WrapFunctionOptions) => {
-      if (options && !options.queue) {
-        return (
-          (...args: Parameters<OmitFirstArg<T2>>) =>
-            getContext()
-              .then(context => func(context, ...args))
-        )
-      }
-      return (
-        (...args: Parameters<OmitFirstArg<T2>>) =>
-          queue
-            .add(() =>
-              getContext()
-                .then(context => func(context, ...args))
-            ) as ReturnType<Awaited<Promise<OmitFirstArg<T2>>>>
-      )
-    }
-
-  return {
-    wrapFunction,
-    setWallet: (_wallet: Wallet | undefined) => {
-      wallet = _wallet
-    },
-    getWallet: () => wallet
-  }
 }
