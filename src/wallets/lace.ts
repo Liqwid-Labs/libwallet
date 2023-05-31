38dfcc5747215111eb83860879544da061db5f11
Seungheon Oh <seungheon.ooh@gmail.com>
2023-05-31 18:18:26 -0500

    Add Lace logo SVG

--- src/wallets/lace.ts
+++ src/wallets/lace.ts
@@ -2,10 +2,12 @@ import type { WalletApi } from '@cardano-sdk/cip30'
 
 import type { Wallet } from '..'
 
+import laceLogo from './assets/lace.svg'
+
 export const name = 'Lace'
 export const origin = 'https://www.lace.io/'
 export const id = 'lace'
-export const icon = 'https://www.lace.io/favicon-32x32.png'
+export const icon = laceLogo
 export const supported = true as const
 
 export const init = (_: { wallet: Wallet, api: WalletApi }) => {
